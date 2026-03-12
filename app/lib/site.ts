f8aec888f2b8b89bb67caf72c850624c852c32df
cosmiclaw1021 <katie.jkwonlaw@gmail.com>
2026-03-12 14:05:31 -0700

    changed the contact email address

--- app/lib/site.ts
+++ app/lib/site.ts
@@ -2,7 +2,7 @@ export const SITE = {
   name: 'Cosmic Law Firm',
   nameSub: 'International Business, Entertainment & Personal Injury Law',
   url: process.env.NEXT_PUBLIC_SITE_URL ?? 'https://cosmiclawfirm.com',
-  email: 'ask@cosmiclawfirm.com',
+  email: 'ask.cosmiclaw@gmail.com',
   phoneDisplay: '(213) 480-0348',
   phoneTel: '2134800348',
   addressShort: '1021 S. Union Ave., Los Angeles, CA 90015',
